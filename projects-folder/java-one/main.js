ce822f902abbd37bb917cb395cc92277debc09fc
tasha kijsamnong <tkijsamnong@yahoo.com>
2014-05-08 00:27:09 -0400

    javahomework

---
+++ projects-folder/java-one/main.js
@@ -0,0 +1,200 @@
+console.log("tasha");
+
+var name = prompt("Hello, my name is very long.");
+	
+	console.log()
+
+
+var age =(26);
+
+
+
+var cats =["frisky","charlie","martin"];
+
+	var add =function(number,cats){
+	
+	return number+cats;
+}
+
+var result =add(1,2);
+
+
+var frisky ={
+	
+	age:10,
+	
+	color: "gray",
+	
+	legs:3,
+	
+	eyes: "yellow",
+	
+	talk: function (){
+		
+		alert("meow");
+	}
+};
+
+
+var martin ={
+	
+	age:2,
+	
+	weight: "fat",
+	
+	legs:4,
+	
+	meow: function (){
+		
+		alert("FEED ME!!!");
+	}
+};
+
+//EXAMPLES:
+
+var answer ="42";
+	answer="THANKS FOR VIEWING.";
+
+	z ="The answer is" + 5
+	T = 5 + "is the answer"
+
+
+
+
+
+
+var ask=prompt("What's your favorite show?");
+
+if (ask=="game of thrones"){
+	
+	console.log ("This show is the shit!");
+
+} else {
+	
+
+	console.log ("that", "is a stupid show!");
+}
+
+
+var listMusic =function(sublime){
+	
+		return ("mgmt","arctic-monkeys");
+}
+
+
+var person ={
+	
+	first:"cliff",
+	
+	last:"Overstreet"
+};
+
+
+var randomArray =["sleepy"];
+
+	var ISpy =["sunglasses","coffee","milk"];
+
+	ISpy [0];
+
+	ISpy [1] ="code";
+
+	ISpy:length;
+
+	ISpy.push ("books");
+
+
+var y =(10);
+
+var x =(20);
+
+var numbers=("math");{
+	sum  =y+x;
+}
+
+
+var NookPlayers =function(personName){
+	
+	return "NookPlayers" + personName;
+}
+
+
+var people =["jane","Charles","Austen"];
+
+	people.push("leni");
+
+	people.push("dakota");
+
+	people.pop();
+
+for (var i=0; i < people.length; i++) {
+	
+	var hiya=NookPlayers (people[i]);
+	
+	("hiya");
+}
+
+
+5&4; //should get 1//
+
+5&4>4%4; //should get true//
+
+5&4<4%4; //should get false//
+
+
+function myObjectType (){
+
+	this.info= "I am a ninja";
+
+	this.showInfo =function(){
+	
+	alert(this.info);
+}
+	}
+
+//test
+function Car(color,year,make){
+		this.color ="black",
+		this.year =1997,
+		this.make ="nissan",
+		this.ododmeterReading =function(newMiles){
+				this.odometerReading =4500;
+		}
+}
+
+var car =new Car("black","1997","Honda",35000);
+
+
+
+ function myFunc(theObject)
+ {
+   theObject.brand = "Honda";
+ }
+ 
+ var mycar = {
+   make: "Honda",
+   model: "civic",
+   year: 2004,
+ };
+
+//test
+function Cliff(x) {
+   function Matt(y) {
+      function Lydia(z) {
+         alert(x + y + z);
+      }
+      Cliff(3);
+   }
+   Matt(2);
+}
+Lydia(1);
+
+function multiply(x, y) {
+   
+   return x * y;
+}
+ 
+
+
+	
+
+
